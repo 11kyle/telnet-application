05aa851fe878d708cea1ea209c6b4b87aeb0a739
Miguel Roman <miguelroman@Miguels-MacBook-Pro.local>
2017-10-25 21:31:18 -0600

    restructuring broke the app. fixed it now

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
-const pad = require('pad');
+const pad = require('./lib/pad');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
