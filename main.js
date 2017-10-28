8d9b31a385148c822952f56b00cad95c704241e5
Kyle <kyle@Kyles-MacBook-Pro.local>
2017-10-28 09:46:50 -0600

    Added Send and Clear buttons to the UI

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 627})
+  mainWindow = new BrowserWindow({width: 800, height: 608})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -26,7 +26,7 @@ function createWindow () {
 
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
